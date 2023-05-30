d3877ae1ce12ea64e95f51d97c0357dbc6b4f0e9
theMustafaADAM <130067257+theMustafaADAM@users.noreply.github.com>
2023-05-30 17:30:38 +0400

    just change background of screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -272,7 +272,7 @@ namespace simple_form_calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkMagenta;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
             this.ClientSize = new System.Drawing.Size(334, 461);
             this.Controls.Add(this.lblfrst);
             this.Controls.Add(this.lblscnd);
